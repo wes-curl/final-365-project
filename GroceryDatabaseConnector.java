4f3e38e89d257b341a2fb7c408b364abb7c1e015
wes-curl <wesleycurl1111@gmail.com>
2022-05-10 13:21:28 -0700

    Started DB connection

---
+++ GroceryDatabaseConnector.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+
+public class GroceryDatabaseConnector {
+    Connection connection;
+    private static String driver = "com.mysql.cj.jdbc.Driver";
+    private static String url = "ambari-node5.csc.calpoly.edu:3306";
+    private static String user = "licurldb";
+    private static String password = "securedpw";
+
+    public GroceryDatabaseConnector(){
+        try{
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getGroceryItems(){
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("");
+            while(resultSet.next()){
+                String row = resultSet.getString(1);
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
